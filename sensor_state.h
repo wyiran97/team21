e863ec727bed95fc237833c2ac29217fe1f9ac58
wyiran97 <42581957+wyiran97@users.noreply.github.com>
2019-06-06 18:58:43 -0400

    Add files via upload

---
+++ sensor_state.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/** 
+  @Course
+    Summer2019 ECE 4534
+ 
+  @File Name
+    debug.h
+
+  @Edited
+    team 21
+ */
+/* ************************************************************************** */
+
+#ifndef _SENSOR_STATE_H    /* Guard against multiple inclusion */
+#define _SENSOR_STATE_H
+
+
+#include <FreeRTOS.h>
+#include "queue.h"
+#include "sensor_queue.h"
+#include "debug.h"
+#include "system_config.h"
+#include "system_definitions.h"
+
+typedef enum {ONE, TWO, THREE, FOUR, FIVE}sensorState;
+
+void runStateMachine(int *sum, Message messageValue, sensorState *currentState);
+void printOutAverage(int average, Message msg);
+
+/* Provide C++ Compatibility */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /* Provide C++ Compatibility */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _EXAMPLE_FILE_NAME_H */
+
+/* *****************************************************************************
+ End of File
+ */
