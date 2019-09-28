cab0b619b3e35bb51524abd0995e00e3320f0e90
wyiran97 <42581957+wyiran97@users.noreply.github.com>
2019-09-28 15:45:58 -0400

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+"""
+A simple echo client
+"""
+import clientKeys
+import socket
+import sys
+from cryptography.fernet import Fernet
+import hashlib
+import pickle
+from ibm_watson import TextToSpeechV1
+from pygame import mixer
+import tweepy
+import time
+
+
+host = sys.argv[2]
+port = int(sys.argv[4])
+size = int(sys.argv[6])
+print("[Checkpoint 01] Connecting to: " + sys.argv[2] + " on port " + sys.argv[4])
+
+
+#capture tweet question
+auth=tweepy.OAuthHandler(clientKeys.APIkey,clientKeys.APISkey)
+auth.set_access_token(clientKeys.Accesstoken,clientKeys.AccessStoken)
+api=tweepy.API(auth)
+
+
+
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self,status):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host,port))
+        str_text=status.text
+        #exclude hash tag
+        question=str_text.replace("#ECE4564T02","")
+        print("[Checkpoint 03] New Question: " + question)
+
+        #construct payload part
+        key = Fernet.generate_key()
+        cipher_suite = Fernet(key)
+        cipher_text = cipher_suite.encrypt(question.encode())
+        checksum = hashlib.md5(cipher_text).hexdigest()
+        print("[Checkpoint 04] Encrypt: Generated Key: " + key.decode() + "| Cipher text: " + cipher_text.decode())
+        payload_tuple = (key, cipher_text, checksum)
+        print("[Checkpoint 05] Sending data: ")
+        print(payload_tuple)
+        payload_pickel = pickle.dumps(payload_tuple)
+        s.send(payload_pickel)
+
+        time.sleep(2)
+
+        #parse answer payload
+        data = s.recv(size)
+        data_unpickle = pickle.loads(data)
+        print("[Checkpoint 06] Received data: ")
+        print(data_unpickle)
+        answer_cipher_text = data_unpickle[1]
+        answer_cipher_suit = Fernet(data_unpickle[0])
+        plain_answer = answer_cipher_suit.decrypt(answer_cipher_text)
+        print("[Checkpoint 07] Decrypt: Using Key: " + data_unpickle[0].decode() + "| Plain text: " + plain_answer.decode())
+
+
+        #specks out answer
+        text_to_speech = TextToSpeechV1(
+            iam_apikey = clientKeys.client_watsonID(),
+            url = clientKeys.client_watsonUrl()
+        )
+
+        with open('answer.mp3', 'wb') as audio_file:
+            audio_file.write(
+                text_to_speech.synthesize(
+                    plain_answer.decode(),
+                    voice='en-US_AllisonVoice',
+                    accept='audio/mp3'
+                ).get_result().content)
+
+        mixer.init()
+        mixer.music.load('answer.mp3')
+        mixer.music.play()
+        print("[Checkpoint 08] Speaking Answer: " + plain_answer.decode())
+        time.sleep(5)
+
+
+        s.close()
+
+
+
+myStreamListener=MyStreamListener()
+myStream=tweepy.Stream(auth = api.auth, listener = myStreamListener)
+print("[Checkpoint 02] Listening for tweets from Twitter API that contain questions")
+data=myStream.filter(track=['#ECE4564T02'])
+
+
+
+
+
+
+
+print ('Received:', plain_answer)
---
+++ clientKeys.py
@@ -0,0 +1,15 @@
+from ibm_watson import TextToSpeechV1
+
+
+APIkey="XBoEiyD2yPCLHQAaZpV7PzZS5"
+APISkey="YgNLXDlNmAZYEdOqD79GO3taCg7iLzsMcyeYP3jXr1uBNKxkgv"
+Accesstoken="1177381780321710082-k2cEwnGgPhaW2s1Q65zHGWsl0LJ9dN"
+AccessStoken="tQmEf43GDv7oqzQZufKn26U7ZsYknZaGA2PArcoOSUuvp"
+
+def client_watsonID():
+    wat_id = "-xdyZg7dTq5g8fktKjGwKmdo3BzYd-WQ8_U-7OCIomLT"
+    return wat_id
+
+def client_watsonUrl():
+    url = "https://gateway-wdc.watsonplatform.net/text-to-speech/api"
+    return url
