aac2f8827a7cfaf473e8ec3c7b7a735e273c9563
zge8 <35852059+zge8@users.noreply.github.com>
2019-09-28 16:45:11 -0400

    Add files via upload

--- Client.py
+++ Client.py
@@ -18,7 +18,7 @@ import time
 host = sys.argv[2]
 port = int(sys.argv[4])
 size = int(sys.argv[6])
-print("[Checkpoint 01] Connecting to: " + sys.argv[2] + " on port " + sys.argv[4])
+print("["+time.strftime("%H:%M:%S",time.localtime())+"][Checkpoint 01] Connecting to: " + sys.argv[2] + " on port " + sys.argv[4])
 
 
 #capture tweet question
@@ -35,16 +35,16 @@ class MyStreamListener(tweepy.StreamListener):
         str_text=status.text
         #exclude hash tag
         question=str_text.replace("#ECE4564T02","")
-        print("[Checkpoint 03] New Question: " + question)
+        print("["+time.strftime("%H:%M:%S",time.localtime())+"][Checkpoint 03] New Question: " + question)
 
         #construct payload part
         key = Fernet.generate_key()
         cipher_suite = Fernet(key)
         cipher_text = cipher_suite.encrypt(question.encode())
         checksum = hashlib.md5(cipher_text).hexdigest()
-        print("[Checkpoint 04] Encrypt: Generated Key: " + key.decode() + "| Cipher text: " + cipher_text.decode())
+        print("["+time.strftime("%H:%M:%S",time.localtime())+"][Checkpoint 04]Encrypt: Generated Key: " + key.decode() + "| Cipher text: " + cipher_text.decode())
         payload_tuple = (key, cipher_text, checksum)
-        print("[Checkpoint 05] Sending data: ")
+        print("["+time.strftime("%H:%M:%S",time.localtime())+"][Checkpoint 05] Sending data: ")
         print(payload_tuple)
         payload_pickel = pickle.dumps(payload_tuple)
         s.send(payload_pickel)
@@ -54,12 +54,12 @@ class MyStreamListener(tweepy.StreamListener):
         #parse answer payload
         data = s.recv(size)
         data_unpickle = pickle.loads(data)
-        print("[Checkpoint 06] Received data: ")
+        print("["+time.strftime("%H:%M:%S",time.localtime())+"][Checkpoint 06] Received data: ")
         print(data_unpickle)
         answer_cipher_text = data_unpickle[1]
         answer_cipher_suit = Fernet(data_unpickle[0])
         plain_answer = answer_cipher_suit.decrypt(answer_cipher_text)
-        print("[Checkpoint 07] Decrypt: Using Key: " + data_unpickle[0].decode() + "| Plain text: " + plain_answer.decode())
+        print("["+time.strftime("%H:%M:%S",time.localtime())+"][Checkpoint 07]Decrypt: Using Key: " + data_unpickle[0].decode() + "| Plain text: " + plain_answer.decode())
 
 
         #specks out answer
@@ -79,7 +79,7 @@ class MyStreamListener(tweepy.StreamListener):
         mixer.init()
         mixer.music.load('answer.mp3')
         mixer.music.play()
-        print("[Checkpoint 08] Speaking Answer: " + plain_answer.decode())
+        print("["+time.strftime("%H:%M:%S",time.localtime())+"][Checkpoint 08] Speaking Answer: " + plain_answer.decode())
         time.sleep(5)
 
 
@@ -89,7 +89,7 @@ class MyStreamListener(tweepy.StreamListener):
 
 myStreamListener=MyStreamListener()
 myStream=tweepy.Stream(auth = api.auth, listener = myStreamListener)
-print("[Checkpoint 02] Listening for tweets from Twitter API that contain questions")
+print("["+time.strftime("%H:%M:%S",time.localtime())+"][Checkpoint 02] Listening for tweets from Twitter API that contain questions")
 data=myStream.filter(track=['#ECE4564T02'])
 
 
